bb2f1c4db92a4b0779c2e6a7ac62336f49b66531
Carl <carldavidroberts1996@gmail.com>
2025-06-29 16:11:32 +0100

    No longer needed, updated repo located in Gitlab

--- quick_scripts/essentials.lua
+++
@@ -1,6 +0,0 @@
-local tag = "essentials"
-
--- Disable high pitched ringing sound effect when damaged by an explosion
-hook.Add("OnDamagedByExplosion", tag, function(ply, dmginfo)
-    return true
-end)
